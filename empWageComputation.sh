a41e68189e7d8b70de72fd78f51625b25a43504e
arjunjava <jadhavarjun53@gmail.com>
2020-12-09 22:23:18 +0530

    Add: code to calculate part time work

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,14 +4,21 @@ echo "Welcome to employee Wage Program"
 
 WagePerHr=20
 FullDayHrs=8
+PartTimeHrs=4
 
-var=$(($RANDOM%2))
+var=$(($RANDOM%3))
 
 if [ $var -eq 1 ]
 then
         dailywages=$(($WagePerHr*$FullDayHrs))
-	echo "The Daily Wage Of Employee Is  : $dailywages"
+	echo "The Full Day Wage Of Employee Is  : $dailywages"
+
+elif [ $var -eq 2 ]
+then
+        dailywages=$(($WagePerHr*$PartTimeHrs))
+        echo "The Part Time Wage Of Employee Is  : $dailywages"
 
 else
         echo "Employee Is Absent"
 fi
+
